1dda5943f29df4ba8d92df89efed233cdffc4528
Colin <zhouchaolin@gmail.com>
2015-11-22 18:36:43 +0800

    scan the port through python-nmap

--- xudong/scan_port.py
+++ xudong/scan_port.py
@@ -1,73 +1,76 @@
 import json
 import nmap
+
+
 # assemble the msg
-def assemble_msg(server_group, host_name, host_ip, status):
-	pass
+def assemble_server_msg(server_group, host, nm_entity):
+        host_name = nm_entity['scan'][host]['hostnames']
+        host_ip = host
+        server_status = nm_entity['scan'][host]['status']['state']
+        ret_msg = "<h2>"
+        ret_msg += "group name: %s " % server_group
+        ret_msg += "host_ip: %s " % host_ip
+        ret_msg += "server_status: %s" % server_status
+        ret_msg += "</h2>\n"
+        return ret_msg
+
+# read a json file
+def read_config(cfg_file):
+        with open(cfg_file,"r") as cfg_file:
+                data = json.load(cfg_file)
+        return data
+
+# scan a host with port_range and white_list
+def scan_host_portrange(nmhandler, hosts, port_range, white_list):
+        ret_msg = ""
+        for host in hosts:
+                t_nm = nmhandler.scan(host, port_range)
+                ret_msg += assemble_server_msg(group_name, host, t_nm)
+                if 'tcp' in t_nm['scan'][host]:
+                        open_ports =  t_nm['scan'][host]['tcp'].keys()
+                if 'udp' in t_nm['scan'][host]:
+                        open_ports += t_nm['scan'][host]['udp'].keys()
+                # open_ports is none
+                if not open_ports:
+                        if not white_list:
+                                ret_msg += '<p>none white_list</p>\n'
+                        else:
+                                ret_msg += '<p>need to scan %s</p>\n' % ','.join(white_list)
+                # open_ports is not none
+                else:
+                        # check the opened ports
+                        for port in open_ports:
+                                if port in white_list:
+                                        ret_msg += '<p>port %s opened OK!</p>\n' % port
+                                else:
+                                        ret_msg += '<p>port %s should closed!</p>\n' % port
+                        # check the white list
+                        for white_port in white_list:
+                                if white_port not in open_ports:
+                                        ret_msg += '<p>port %s should opened</p>\n' % white_port
+        return ret_msg
 
 # write into the file
-
+def write_to_html(input_str, file_name):
+        # wrap the file info
+        html_wrap = '<html>\n<head>Port Scan Result</head>\n<body>\n' + input_str + '</body></html>\n'
+        with open(file_name, 'w') as html_file:
+                html_file.write(html_wrap)
 
 
 
 if __name__ == "__main__":
 	# read the config
-	with open("config.txt","r") as cfg_file:
-		data = json.load(cfg_file)
+        data = read_config('config.txt')
 	# read tasks
 	tasks = data["tasks"]
+        nm = nmap.PortScanner()
+        ret_msg = ""
 	for i in range(len(tasks)):
-		group_name = task[i]["group_name"]
-		hosts = tasks[i]["ip_list"]
+		group_name = tasks[i]["group_name"]
+		hosts = map(str, tasks[i]["ip_list"])
 		port_range = tasks[i]["port_range"]
-		white_list = tasks[i]["white_list"]
-		for host in hosts:
-			nm = nmap.PortScanner()
-			tmp = nm.scan(host, port_range)
-			assemble_msg(group_name, tmp['scan'][host]['hostname'], host, tmp['scan'][host]['status']['state'])
-			try:
-				ports = tmp['scan'][host]['tcp'].keys()
-			except KeyError, e:
-				if white_list:
-					white_str = ','.join(white_list)
-					ret_msg = "None opened port"
-				else:
-					ret_msg = "Without any Problem"
-
-
-
-
-
-
-
-def nmScan(hostlist,portrange,whitelist): 
-    p = re.compile("^(\d*)\-(\d*)$") 
-    if type(hostlist) != list: 
-            help() 
-        portmatch = re.match(p,portrange) 
-    if not portmatch: 
-            help() 
-        l = [] 
-        for host in hostlist: 
-            result = '' 
-        nm = nmap.PortScanner() 
-            tmp = nm.scan(host,portrange) 
-            result = result + "<h2>ip地址:%s 主机名:[%s]  ......  %s</h2><hr>" %(host,tmp['scan'][host]['hostname'],tmp['scan'][host]['status']['state']) 
-            try: 
-                ports = tmp['scan'][host]['tcp'].keys() 
-            except KeyError,e: 
-                if whitelist: 
-                    whitestr = ','.join(whitelist) 
-                    result = result + "未扫到开放端口!请检查%s端口对应的服务状态" %whitestr  
-                else: 
-                    result = result + "扫描结果正常，无暴漏端口"
-                    continue
-            for port in ports: 
-              info = '' 
-              if port not in whitelist: 
-                  info = '<strong><font color=red>Alert:非预期端口</font><strong>&nbsp;&nbsp;' 
-              else: 
-                  info = '<strong><font color=green>Info:正常开放端口</font><strong>&nbsp;&nbsp;'
-              portinfo = "%s <strong>port</strong> : %s &nbsp;&nbsp;<strong>state</strong> : %s &nbsp;&nbsp;<strong>product<strong/> : %s <br>" %(info,port,tmp['scan'][host]['tcp'][port]['state'], 
-                                                                        tmp['scan'][host]['tcp'][port]['product']) 
-              result = result + portinfo 
-            l.append([host,str(result)]) 
+		white_list = map(int, tasks[i]["white_list"])
+                ret_msg += scan_host_portrange(nm, hosts, port_range, white_list)
+        # write the html
+        write_to_html(ret_msg, 'scan_res.html')
