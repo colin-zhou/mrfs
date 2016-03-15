96c4eda2c221abcccf3ff2c264190d4ae400ad65
Zhou Chaolin <zhouchaolin@mycapital.net>
2016-03-15 20:44:21 +0800

    log and process event listen
    1. sls file parser
    2. listen event manager

--- python/tail/taillog.py
+++ python/tail/taillog.py
@@ -104,39 +104,11 @@ class my_event(object):
         return self.info == level
 
 
-@singleton
-class event_handlers(object):
-
-    def __init__(self):
-        self._handlers = {}
-
-    def add_handler(event, func, params=None):
-        if event in self._handlers:
-            return False
-        else:
-            self._handlers[event] = [func, params]
-            return True
-
-    def del_hanlder(event):
-        if event in self._handlers:
-            del self._handlers[event]
-            return True
-        else:
-            return False
-
-    def trigger_event(event):
-        if event in self._handlers:
-            fp = self._handlers[event]
-            return fp[0](fp[1])
-        return None
-
-
 class event_manager(threading.Thread):
 
     def __init__(self):
         self.e_queue = Queue()
         self.stop = False
-        self.handlers = event_handlers.Instance()
         self.cache_arr = []
         self.cnt = 0
         threading.Thread.__init__(self)
@@ -144,7 +116,7 @@ class event_manager(threading.Thread):
     def stop(self):
         self.stop = True
 
-    def test_fun(self, event):
+    def print(self, event):
         print event
 
     def get_event_queue(self):
@@ -157,14 +129,14 @@ class event_manager(threading.Thread):
                 if not isinstance(pro_event, my_event):
                     my_log.error("pro_event type error")
                 else:
-                    self.test_fun(pro_event)
+                    self.print(pro_event)
             else:
                 self.cnt += 1
                 sleep(1)
 
 @singleton
 class event_checkout(object):
-    
+
     """
     Example:
     2016-03-01 15:25:33.180600759 agent.dbg.0.7.0/17678 oss TRACE start
@@ -232,11 +204,9 @@ class analysis_log(object):
         self.filename = logfile
         self.program = program
         self.event_manager = event_manager()
-        self.event_handler = event_handlers.Instance()
         self.event_queue = self.event_manager.get_event_queue()
         self.event_check = event_checkout.Instance()
         self.event_check.init_queue(self.event_queue)
-        
 
         t = my_event(name="default", program="agent", level="trace",
                      info="", ltime="")
@@ -244,9 +214,6 @@ class analysis_log(object):
         self.event_check.add_event(t)
 
         self.event_manager.start()
-
-
-
 
     def line_analysis(self, in_line):
         self.event_check.line_process(in_line, self.program)
