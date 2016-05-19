f7912eabab3fb13b584c231048a9174290754611
Zhou Chaolin <zhouchaolin@mycapital.net>
2016-05-19 08:14:39 +0800

    linux api test

--- linux_api/assert/test.c
+++ linux_api/assert/test.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <assert.h>
+#include <math.h>
+#include <stdlib.h>
+
+double my_sqrt(double x)
+{
+    assert(x >= 0.0);
+    return sqrt(x);
+}
+
 
 
 int main()
 {
-    int c = 0;
-    assert(c);
+    printf("sqrt +2 = %g\n", my_sqrt(2.0));
+    printf("sqrt -2 = %g\n", my_sqrt(-2.0));
     return 0;
 }
