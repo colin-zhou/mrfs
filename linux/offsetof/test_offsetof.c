962e91017524d5f01deff1543be4a5ebf39784ca
Zhou Chaolin <zhouchaolin@mycapital.net>
2016-12-14 17:28:47 +0800

    Fix bugs in test_offsetof

--- linux/offsetof/test_offsetof.c
+++ linux/offsetof/test_offsetof.c
@@ -18,8 +18,8 @@ main(void)
     test.c = 'h';
     test.a = he;
 
-    struct s *tmp =(struct s *)(test.a - offsetof(struct s, a));
-    printf("the right value i= %d\n", tmp->i);
+    struct s *tmp =(struct s *)((void *)&test.a - offsetof(struct s, a));
+    printf("the right value i= %d, c=%c\n", tmp->i, tmp->c);
 
     printf("offsets: i=%zd; c=%zd; d=%zd a=%zd\n",
             offsetof(struct s, i), offsetof(struct s, c),
