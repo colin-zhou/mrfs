1b0b6e9e343eaa26b68c70c8e0d3343f6bd73096
Zhou Chaolin <zhouchaolin@mycapital.net>
2017-09-12 12:56:10 +0800

    Add md5sum and intip converter

--- python/socket/intip.py
+++ python/socket/intip.py
@@ -3,7 +3,7 @@
 """
 bidirection convert ip and int
 """
-
+import struct
 import socket
 
 
@@ -17,3 +17,16 @@ def inttoip(ip):
 
 def iptoint(ip):
     return int(socket.inet_aton(ip).encode('hex'), 16)
+
+
+def ip2int(addr):
+    return struct.unpack("!I", socket.inet_aton(addr))[0]
+
+
+def int2ip(addr):
+    return socket.inet_ntoa(struct.pack("!I", addr))
+
+
+if __name__ == "__main__":
+    print(int2ip(1428072640))
+    print(int2ip(16777343))
