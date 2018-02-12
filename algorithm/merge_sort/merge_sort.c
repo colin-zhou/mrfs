a9f528ccb52cb6692c205d71ec65b76a74b6f6d4
Zhou Chaolin <zhouchaolin@yeah.net>
2018-02-12 18:05:40 +0000

    Make the merge_method compilable

--- algorithm/merge_sort/merge_sort.c
+++ algorithm/merge_sort/merge_sort.c
@@ -1,29 +1,24 @@
 #include <stdio.h>
+#include <limits.h>
+#include "merge_sort.h"
 
-
-#define MAX_CHANNEL_CNT (4096)
-#define encode_tick(a, b) (a)<<12 + (b)
-#define decode_tick_col(v) (0xFFFFF000 & a) >> 12
-#define decode_tick_row(v) (0xFFF & v)
-
-enum _SORT_METHOD {
-    LOCAL_TIME=0,
-    EXCH_TIME=1,
-} SORT_METHOD;
+enum SORT_METHOD {
+    BY_LOCAL_TIME=0,
+    BY_EXCH_TIME=1
+};
 
 /***
  * Max support 4096 channel quote in order
  */
-
-typedef quote_tick {
+typedef struct quote_tick {
     uint64_t exch_time;
     uint64_t local_time;
     uint32_t column;
-    uint32_t index;
+    uint32_t row;
 } quote_tick_t;
 
 
-typedef common_quote {
+typedef struct common_quote {
     int serial;
     int mi_type;
     uint64_t exch_time;
@@ -31,30 +26,41 @@ typedef common_quote {
 } common_quote_t;
 
 
-typedef loser_index_t size_t;
+typedef struct channel_data {
+    void *data;
+    int itemsize;
+    int size; 
+} channel_data_t;
 
-typedef g_quote {
-    int size;
-    quote_tick_t q[MAX_CHANNEL_CNT + 1]; /* quote items q[k]->mininum value*/
-    loser_index  l[MAX_CHANNEL_CNT + 1]; /* loser index l[0]->champion value*/
+typedef size_t loser_index_t;
+
+typedef struct g_quote {
+    int            size;
+    int            sort_method;
+    uint64_t       p_tick_id;              /* previous return index */
+    quote_tick_t   q[MAX_CHANNEL_CNT + 1]; /* quote items q[k]->mininum value*/ 
+    loser_index_t  l[MAX_CHANNEL_CNT + 1]; /* loser index l[0]->champion value*/
+    channel_data_t d[MAX_CHANNEL_CNT + 1]; /* channel data information */
 } g_quote_t;
 
-g_quote_t all_quote_data;
+g_quote_t    all_quote_data;
+quote_tick_t d_max_quote_tick;
 
 void
 adjust_by_exch_time(int s)
 {
     int i;
     for(i = (s + all_quote_data.size) / 2; i > 0; i = i / 2) {
-        /* i is father node */
-        if (all_quote_data.q[s].exch_time > b[all_quote_data.l[i]].exch_time)
+        /* champion node bigger than father node */
+        if (all_quote_data.q[s].exch_time > all_quote_data.q[all_quote_data.l[i]].exch_time)
         {
             /* swap ls[i] and s */
-            ls[i] = s;
-            s^ = ls[i];
-            ls[i] = s;
+            all_quote_data.l[i] = s;
+            s ^= all_quote_data.l[i];
+            all_quote_data.l[i] = s;
         }
     }
+    /* the champion of the adjustion */
     all_quote_data.l[0] = s;
 }
 
@@ -63,67 +69,124 @@ adjust_by_local_time(int s)
 {
     int i;
     for(i = (s + all_quote_data.size) / 2; i> 0; i = i / 2) {
-        if (all_quote_data.q[s].local_time > b[all_quote_data.l[i]].local_time)
+        /* champion node bigger than father node */
+        if (all_quote_data.q[s].local_time > all_quote_data.q[all_quote_data.l[i]].local_time)
         {
-            ls[i] = s;
-            s^ = ls[i];
-            ls[i] = s;
+            /* swap ls[i] and s */
+            all_quote_data.l[i] = s;
+            s ^= all_quote_data.l[i];
+            all_quote_data.l[i] = s;
         }
     }
+    /* the champion of the adjustion */
     all_quote_data.l[0] = s;
 }
 
 void
-convert_to_quote_tick(void *squote, quote_tick_t *dquote, int index, int col)
+convert_to_quote_tick(void *squote, quote_tick_t *dquote, int column, int row)
 {
-    dquote->colume = col;
-    dquote->index = index;
-    dquote->local_time = squote->local_time;
-    dquote->exch_time = squote->exch_time;
-}
-
-void
-process_col_finished(int index)
-{
+    common_quote_t *cq = (common_quote_t *)squote;
+    dquote->column = column;
+    dquote->row = row; 
+    dquote->local_time = cq->local_time;
+    dquote->exch_time = cq->exch_time;
 }
 
 /**
  * multi sorted list of tick_sort_item_t 
  */
-void
+static void
 init_loser_tree(int sort_method)
 {
     int i;
-    /* only work on 64bit compiler */
-    all_quote_data.q[all_quote_data.size] = {ULONG_MAX, ULONG_MAX, 0, 0};
+    d_max_quote_tick = {UINT_MAX, UINT_MAX, (uint32_t)-1, (uint32_t)-1};
+    all_quote_data.p_tick_id = -1;
+    /* compare item */
+    all_quote_data.q[all_quote_data.size] = {0, 0, (uint32_t)-1, (uint32_t)-1};
     /* all index point to the largest value */
     for (i = 0; i < all_quote_data.size + 1; i++)
         all_quote_data.l[i] = all_quote_data.size;
-    if (sort_method == LOCAL_TIME) {
-        for (i = 0; i < all_quote_data.size; i++)
+    if (sort_method == BY_LOCAL_TIME) {
+        for (i = all_quote_data.size - 1; i > 0; i--)
             adjust_by_local_time(i);
     } else {
-        for (i = 0; i < all_quote_data.size; i++)
+        for (i = all_quote_data.size - 1; i > 0; i--)
             adjust_by_exch_time(i);
     }
 
 }
 
 /**
- * pop tick data
+ * fetch quote tick in array and fill it into tick
+ * pointed memory
  */
-int
-pop_tick()
+static void
+fetch_tick(int column, int row, quote_tick_t *tick)
 {
 }
-
 
 /**
- * insert tick data
+ * update the loser tree after remove one item
  */
-int
-insert_tick()
+static void
+update_loser_tree()
 {
+    int col, row;
+    col = decode_tick_col(all_quote_data.p_tick_id);
+    row = decode_tick_row(all_quote_data.p_tick_id);
+    if (row < all_quote_data.d[col].size) {
+        /* more quote in the queue */
+        //all_quote_data.q[col] = all_quote_data.q[col+1];
+        convert_to_quote_tick(all_quote_data.d[col].data, &all_quote_data.q[col], col, row+1);
+    } else {
+        all_quote_data.q[col] = d_max_quote_tick;
+    } 
+    if (all_quote_data.sort_method == BY_LOCAL_TIME) {
+        adjust_by_exch_time(col);
+    } else {
+        adjust_by_local_time(col);
+    }
 }
 
+/**
+ * pop tick data
+ */
+int64_t
+pop_tick()
+{
+    /* return an int value identify the column and row */
+    int64_t col, row;
+    col = all_quote_data.q[0].column;
+    row = all_quote_data.q[0].row;
+    update_loser_tree();
+    if (col == -1) {
+        /* None */
+        return -1; 
+    }
+    return encode_tick(col, row);
+}
 
+/**
+ * load quote locally
+ */
+void
+load_quote(void *data, int sort_method)
+{
+    /* load the quote channel into cell array */
+    /* init all necessary */
+    init_loser_tree(sort_method); 
+}
+
+void
+load_quote_c(void *data, int size, int sort_method)
+{
+    int j = 0;
+    channel_data_t *d;
+    for (int i = 0; i < size; i++) {
+        d = &all_quote_data.d[i];
+        d->data = data;
+        d->size = 100;
+    }
+    init_loser_tree(sort_method);
+
+}
--- algorithm/merge_sort/test.c
+++ algorithm/merge_sort/test.c
@@ -1,48 +1,79 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "merge_sort.h"
 
-using namespace std;
 
-#define LEN 5//败者树容量，多路归并数目
-#define MIN -1//所有数据的可能最小值
+typedef struct channel_data {
+    void *data;
+    int itemsize;
+    int size; 
+} channel_data_t;
 
-int ls[LEN+1];//败者树,ls[0]存放胜者，其余存放败者
-int buf[LEN+1];//存放多路归并的头元素值,多出来的一位放MIN
+typedef struct common_quote {
+    int serial;
+    int mi_type;
+    uint64_t exch_time;
+    uint64_t local_time;
+} common_quote_t;
 
-void adjust(int s,int *buf){//s是需要调整的buf的下标
-    int t=(s+LEN)/2;//得到s的在败者树上面的父节点
-    while(t>0){//不断与父节点对比，直到败者树的根节点
-        if(buf[s]>buf[ls[t]]){//如果当前节点s（胜者）大于父节点
-            ls[t]^=s;//交换ls[t]和s
-            s^=ls[t];//s记录胜者
-            ls[t]^=s;//父节点记录败者
-        }
-        t/=2;//得到败者树的上一个父节点，继续与当前胜者s比较
+typedef struct quote_tick {
+    uint64_t exch_time;
+    uint64_t local_time;
+    uint32_t column;
+    uint32_t row;
+} quote_tick_t;
+
+
+
+channel_data_t *
+get_random_data(int size)
+{
+    channel_data_t *d = (channel_data_t *)malloc(sizeof(channel_data_t));
+    quote_tick_t *tick = (quote_tick_t *)malloc(sizeof(quote_tick_t) * size);
+    d->data = (void*) tick;
+    d->itemsize = sizeof(common_quote_t);
+    for(int i = 0; i < size; i++) {
+       	tick[i].local_time = rand() % 100000; 
+        tick[i].exch_time = tick->local_time;
     }
-    ls[0]=s;//最终的胜者记录于ls[0]
+    d->size = size;
+    return d;
 }
 
-void build(int *buf){
-    buf[LEN]=MIN;//最后一位放MIN
-    int i;
-    for(int i=0;i<LEN+1;++i)
-        ls[i]=LEN;//所有败者树初始化为MIN的下标
-    for(i=0;i<LEN;++i)
-        adjust(i,buf);//依次调整即可完成初始化
+void
+test_load_quote()
+{
+    channel_data_t *d1 = get_random_data(100);
+    channel_data_t *d2 = get_random_data(100);
+    channel_data_t *d3 = get_random_data(100);
+    channel_data_t *d4 = get_random_data(100);
+    void *x[4];
+    x[0] = d1;
+    x[1] = d2;
+    x[2] = d3;
+    x[2] = d4;
+    load_quote_c(x, 4, 0);
+}
+
+
+void
+init_srand()
+{
+    //srand((unsigned) time(0));
 }
 
 int main()
 {
-    //初始buf
-    int tmp[5]={18,21,16,11,19};
-    memcpy(buf,tmp,LEN*sizeof(int));
-    build(buf);
-    cout<<buf[ls[0]]<<endl;//输出11
-
-    //取出11后，buf[3]=17
-    int tmp1[5]={18,21,16,17,19};
-    memcpy(buf,tmp1,LEN*sizeof(int));
-    adjust(3,buf);
-    cout<<buf[ls[0]]<<endl;//输出16
-
-    return 0;
+    init_srand();
+    test_load_quote();
+    int row, col;
+    int64_t res;
+    int idx = 0;
+    while ((res = pop_tick()) != -1) {
+        idx++;
+        row =  decode_tick_row(res);
+        col = decode_tick_col(res);
+        printf("idx = %d, row = %d, col = %d\n", idx, row, col);
+    }
 }
