a97ad65708566a6ea025b7bbb779aca8fb611bfc
Zhou Chaolin <zhouchaolin@yeah.net>
2018-02-23 06:05:03 -0500

    Move the file to another position

--- test/benchmark/network_card/common_command.sh
+++ test/benchmark/network_card/common_command.sh
@@ -5,6 +5,7 @@
 echo "password" | sudo -S sysctl -m vm.drop_caches=3
 
 # start monitor(transmite, receive)
+# install with pip install speedometer (python2.7)
 speedometer -r wlan0 -t wlan0
 
 # bind core
