88e9aa8a83ec79e5efc559445449f65c7a547c67
Zhou Chaolin <zhouchaolin@mycapital.net>
2018-11-29 13:38:11 +0800

    Update vim configuration and provide some common util

--- python/utils/decorator.py
+++ python/utils/decorator.py
@@ -18,6 +18,17 @@ def perf_time(func):
     return wrapper
 
 
+def perf_time_b(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        res = func(*args, **kwargs)
+        consume = time.perf_counter() - start
+        print("function: ", func.__name__, "consume : %s seconds" % consume)
+        return res
+    return wrapper
+
+
 def singleton(cls):
     """
     singleton decorator
@@ -49,3 +60,15 @@ def exec_per_day(func):
             }
         return pre_exec[flat_key]['result']
     return wrapper
+
+def debug(func):
+    """Print the function signature and return value"""
+    @functools.wraps(func)
+    def wrapper_debug(*args, **kwargs):
+        args_repr = [repr(a) for a in args]
+        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
+        signature = ", ".join(args_repr + kwargs_repr)
+        print(f"Calling {func.__name__}({signature})")
+        value = func(*args, **kwargs)
+        print(f"{func.__name__!r} returned {value!r}")
+        return value
