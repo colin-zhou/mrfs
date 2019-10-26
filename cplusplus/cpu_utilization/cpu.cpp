d51951237ecd00a223cdfb8a61f7d5c9ae351a6b
Zhou Chaolin <zhouchaolin@gmail.com>
2019-10-26 16:16:03 +0800

    Add more c++ code

--- cplusplus/cpu_utilization/cpu.cpp
+++ cplusplus/cpu_utilization/cpu.cpp
@@ -3,17 +3,17 @@
 #include <numeric>
 #include <unistd.h>
 #include <vector>
- 
+
 std::vector<size_t> get_cpu_times() {
     std::ifstream proc_stat("/proc/stat");
     proc_stat.ignore(5, ' '); // Skip the 'cpu' prefix.
     std::vector<size_t> times;
     for (size_t time; proc_stat >> time; times.push_back(time)){
-        std::cout << time <<std::endl;
+        /* std::cout << time <<std::endl; */
     }
     return times;
 }
- 
+
 bool get_cpu_times(size_t &idle_time, size_t &total_time) {
     const std::vector<size_t> cpu_times = get_cpu_times();
     if (cpu_times.size() < 4)
@@ -22,7 +22,7 @@ bool get_cpu_times(size_t &idle_time, size_t &total_time) {
     total_time = std::accumulate(cpu_times.begin(), cpu_times.end(), 0);
     return true;
 }
- 
+
 int main(int, char *[]) {
     size_t previous_idle_time=0, previous_total_time=0;
     for (size_t idle_time, total_time; get_cpu_times(idle_time, total_time); sleep(1)) {
