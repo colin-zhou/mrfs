05208e4a4c969f256459446566f0106ed9b32f8d
colin <zhouchaolin@yeah.net>
2019-12-09 11:08:59 +0800

    Add a chunk function

--- python/utils/func.py
+++ python/utils/func.py
@@ -92,3 +92,11 @@ def get_local_internet_ip():
     p.connect(('www.baidu.com', 80))
     return p.getsockname()[0]
 
+
+def chunk_list(lst, n):
+    """
+    Yield successive n-sized chunks from lst.
+    """
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
